4f12a8e838e8f7e8981913da44fd2cbd11f222d7
Peron <cristian.vidalsastre@gmail.com>
2019-01-29 11:26:44 -0300

    my first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app id="inspire">
+    <prueba-template></prueba-template>
+  </v-app>
+</template>
+
+<script>
+import PruebaTemplate from "./components/PruebaTemplate";
+export default {
+  data: () => ({}),
+  components: { "prueba-template": PruebaTemplate },
+  props: {}
+};
+</script>
---
+++ src/components/PruebaTemplate.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-app id="inspire">
+
+    <v-container
+      grid-list-md
+      text-xs-left
+    >
+      <v-layout
+        row
+        wrap
+      >
+
+        <v-flex xs12>
+          <v-card
+            dark
+            color="primary"
+          >
+            <v-card-text class="px-0">
+              <v-btn icon>
+                <v-icon>visibility</v-icon>
+              </v-btn>Titulo
+            </v-card-text>
+            <v-card-text class="px-0">
+              <v-btn icon>
+                <v-icon>visibility</v-icon>
+              </v-btn>Codigo
+            </v-card-text>
+            <v-card-text class="px-0">
+              <v-btn icon>
+                <v-icon>visibility</v-icon>
+              </v-btn>Apellido
+            </v-card-text>
+            <v-card-text class="px-0">
+              <v-btn icon>
+                <v-icon>visibility</v-icon>
+              </v-btn>Nombre
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs6>
+          <v-card
+            dark
+            color="secondary"
+          >
+            <v-card-text class="px-0">
+              <v-btn icon>
+                <v-icon>
+                  calendar_today</v-icon>
+              </v-btn>Fecha
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs6>
+          <v-card
+            dark
+            color="secondary"
+          >
+            <v-card-text class="px-0">
+              <v-btn icon>
+                <v-icon>monetization_on</v-icon>
+              </v-btn>Monto
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs6>
+          <v-card
+            dark
+            color="secondary"
+          >
+            <v-card-text class="px-0">
+              <v-btn icon>
+                <v-icon>
+                  calendar_today</v-icon>
+              </v-btn>Fecha
+            </v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs6>
+          <v-card
+            dark
+            color="secondary"
+          >
+            <v-card-text class="px-0">
+              <v-btn icon>
+                <v-icon>monetization_on</v-icon>
+              </v-btn>Monto
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    source: String
+  }
+};
+</script>
